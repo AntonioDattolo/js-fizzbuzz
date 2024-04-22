8a231226fb5bb51670aab364518cd0532c454d8f
AntonioDattolo <antonio19794.a@gmail.com>
2024-04-22 11:29:13 +0200

    another revision

--- js/main.js
+++ js/main.js
@@ -2,40 +2,29 @@ const container = document.querySelector('div');
 
 for (let i = 1; i <= 100; i++) {
 
-    if (i % i == 0 && i % 3 != 0 && i % 5 != 0 ) {
-        console.log(i);
-        const newItem = document.createElement('h6');
-        container.append(newItem);
-        newItem.innerHTML =` ${i}`;
-        newItem.classList.add('m-1');
-        newItem.classList.add('my-witdh');  
-    }
-    else if (i % 5 == 0 && i % 3 == 0) {
+    const newItem = document.createElement('h6');
+    container.append(newItem);
+    newItem.classList.add('m-1');
+    newItem.classList.add('my-witdh'); 
+
+    if (i % 5 == 0 && i % 3 == 0) {
         console.log("fizzbuzz");
-        const newItem = document.createElement('h6');
-        container.append(newItem);
         newItem.innerHTML = "FizzBuzz";
-        newItem.classList.add('m-1');
-        newItem.classList.add('my-witdh');
         newItem.classList.add('fizzbuzz');
     }
     else if (i % 3 == 0) {
         console.log("fizz");
-        const newItem = document.createElement('h6');
-        container.append(newItem);
         newItem.innerHTML = " fizz ";
-        newItem.classList.add('m-1');
-        newItem.classList.add('my-witdh');
         newItem.classList.add('fizz');
     }
     else if (i % 5 == 0) {
         console.log("buzz");
-        const newItem = document.createElement('h6');
-        container.append(newItem);
         newItem.innerHTML = " buzz ";
-        newItem.classList.add('m-1');
-        newItem.classList.add('my-witdh');
         newItem.classList.add('buzz');
     }
-   
+    else if (i % i == 0 && i % 3 != 0 && i % 5 != 0 ) {
+        console.log(i);
+        newItem.innerHTML =` ${i}`;
+    }
+
 }
